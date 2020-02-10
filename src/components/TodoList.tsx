723f51c7dafac30d7d84c5cea5dc57eb02b8fa89
Jonathan Robbins <jonathan.robbins@rcn.org.uk>
2020-02-10 12:37:55 +0000

    additional state management passing functions as props

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React from "react";
 
 interface TodoListProps {
-    items: {id: string, text: string}[];
+  items: { id: string; text: string }[];
+  onDeleteTodo: (id: string) => void;
 }
 
 const TodoList: React.FC<TodoListProps> = props => {
-    return (
+  return (
     <ul>
-        {props.items.map(todo => <li key={todo.id}>{todo.text}</li>)}
-    </ul>);
+      {props.items.map(todo => (
+        <li key={todo.id}>
+            <span>{todo.text}</span>
+            <button onClick={props.onDeleteTodo.bind(null, todo.id)}>Delete</button>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default TodoList;
