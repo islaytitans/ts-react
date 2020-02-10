eef2cead895bddb1a8cc4a6bc5dfd343a97ef383
Jonathan Robbins <jonathan.robbins@rcn.org.uk>
2020-02-10 12:48:10 +0000

    added react router and the type definition

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import TodoList from "./components/TodoList";
 import NewTodo from "./components/NewTodo";
 import { Todo } from "./todo.model";
+import { Route } from 'react-router-dom';
 
 const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
